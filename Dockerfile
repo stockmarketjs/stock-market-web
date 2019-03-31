49a649efbc45a71c786e02923af4896ea17d4cc8
ZhouYu <muyu.zhouyu@outlook.com>
2019-04-01 00:25:43 +0800

    第一版

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM keymetrics/pm2:8-alpine as builder
+MAINTAINER  zhouyu muyu.zhouyu@outlook.com
+COPY package.json .
+COPY package-lock.json .
+COPY src src/
+COPY web.conf .
+COPY .eslintrc.js .
+COPY .editorconfig .
+RUN npm install
+RUN npm run build
+
+FROM daocloud.io/library/nginx:1.13.0-alpine
+MAINTAINER  zhouyu muyu.zhouyu@outlook.com
+COPY --from=builder dist dist/
+COPY --from=builder web.conf etc/nginx/conf.d/
--- vue.config.js
+++ vue.config.js
@@ -4,12 +4,12 @@ module.exports = {
         // 设置主机地址
         host: 'localhost',
         // 设置默认端口
-        port: 8080,
+        port: process.env.PORT || 8080,
         // 设置代理
         proxy: {
             '/api': {
                 // 目标 API 地址
-                target: 'http://127.0.0.1:3000/',
+                target: process.env.NODE_ENV === 'prod' ? 'http://api.muyu.party:3000/' : 'http://127.0.0.1:3000/',
                 pathRewrite: {
                     '^/api': ''
                 }
