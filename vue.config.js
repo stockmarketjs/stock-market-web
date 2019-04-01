ff607662c93c6c2127a1b2bd4b4e707eb29406a6
ZhouYu <muyu.zhouyu@outlook.com>
2019-04-01 23:42:15 +0800

    test: nginx

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
         proxy: {
             '/api': {
                 // 目标 API 地址
-                target: process.env.NODE_ENV === 'prod' ? 'http://api.muyu.party:3000/' : 'http://127.0.0.1:3000/',
+                target: 'http://127.0.0.1:3000/',
                 pathRewrite: {
                     '^/api': ''
                 }
