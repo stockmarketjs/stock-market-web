3587e350dba7797cbe993c700d05e65e99728ba3
ZhouYu <muyu.zhouyu@outlook.com>
2019-04-03 22:31:15 +0800

    fix: 文字

--- src/components/stocks/stock.vue
+++ src/components/stocks/stock.vue
@@ -3,7 +3,7 @@
     <p>{{stock.name}}</p>
     <el-tag>市场 {{stock.market==='sh'?'沪市':'深市'}}</el-tag>
     <el-tag>当前价 {{stock.currentPrice}}</el-tag>
-    <el-tag>换手 {{stock.change}}</el-tag>
+    <el-tag>涨幅 {{stock.change}}</el-tag>
     <el-tag>总手 {{stock.totalHand}}</el-tag>
     <el-tag>开盘价 {{stock.startPrice}}</el-tag>
     <el-tag>收盘价 {{stock.endPrice}}</el-tag>
